5aa1449109dedc6d2503057c1fc6f42482bb6ff0
Vlad_Work <inf.vlad.s@bk.ru>
2024-05-22 16:32:45 +0300

    Изменение значений по умолчанию

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'database')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:////tmp/test')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'password')
